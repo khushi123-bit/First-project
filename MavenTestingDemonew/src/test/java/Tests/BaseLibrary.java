338cc4c7026d17684e7d745591368c5d20ad4a99
khushi123-bit <khushbooyadav756503@gmail.com>
2026-03-10 14:03:50 +0530

    Add files via upload

---
+++ MavenTestingDemonew/src/test/java/Tests/BaseLibrary.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseLibrary 
+{ 
+	@Test
+	
+	public void launchUrl()
+	{
+		//WebDriverManager.chromedriver().setup();
+	 WebDriver driver = new ChromeDriver();
+	}
+}
